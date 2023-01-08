02cae06405e980c2e46c1904bd4b145827e9cf96
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-01-09 02:56:39 +0300

    Add project files.

---
+++ AnimeSite/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeSite.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
