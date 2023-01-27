c9507d3b49d8d4dc335c43fbc5fe8c1bb03e7b85
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-01-27 16:45:23 +0300

    dashboard-category

---
+++ AnimeSite/Views/ViewComponents/_CategoryList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeSite.Views.ViewComponents
+{
+    public class _CategoryList:ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.TGetList();
+            return View(values);
+        }
+    }
+}
