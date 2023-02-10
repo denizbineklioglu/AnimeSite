8e695e1a631859887596804afd6e774f263877f0
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-02-10 16:34:16 +0300

    tasarımsal felaketler

--- AnimeSite/Controllers/RecomRobotController.cs
+++ AnimeSite/Controllers/RecomRobotController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AnimeSite.Controllers
 {
     [AllowAnonymous]
     public class RecomRobotController : Controller
     {
+        private readonly ICategoryService _categoryService;
+        public RecomRobotController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+    
         public IActionResult Index()
         {
+            List<SelectListItem> categories = (from x in _categoryService.TGetList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value = x.CategoryID.ToString(),                
+                                               }
+                                               ).ToList();
+            ViewBag.categoryValue = categories;
             return View();
         }
     }
